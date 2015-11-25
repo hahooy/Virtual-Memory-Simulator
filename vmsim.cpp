257aef92c98726e27d6c2521a5944347f5436842
Yongzheng Huang <iamhuangyz@gmail.com>
2015-11-25 17:27:00 -0600

    implemented LRU OPT CLOCK

--- vmsim.cpp
+++ vmsim.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <set>
 #include <algorithm>
 
 using namespace std;
@@ -11,17 +12,26 @@ using namespace std;
 typedef enum {OPT, LRU, FIFO, CLOCK} Algo;
 
 /* function declaration */
-int next_FIFO(int cur_index);
-void pframe(int i, bool page_fault);
+int next_FIFO(int cur_index, int ref_index);
+int next_CLOCK(int cur_index, int ref_index);
+int next_OPT(int cur_index, int ref_index);
+int next_LRU(int cur_index, int ref_index);
+void print_frame(int i, bool page_fault);
+void run();
+void getRef();
 
 /* variable declaration */
 int num_frames;
 string input_file;
 Algo algorithm;
+vector<bool> usebits;
 vector<int> references;
 vector<int> frames;
+set<int> s;
 
-/* function definition */
+/* function definitions */
+
+/* get references from the input file and store them into an array called frames */
 void getRef() {
     string line;
     ifstream infile(input_file);
@@ -38,42 +48,59 @@ void getRef() {
     }
 }
 
+/* simulate the memory page reference */
 void run() {
-    int (* next)(int cur_index);
-    int next_index = 0;
+    int (* next)(int curr_index, int ref_index);
+    int replace_index = -1;
     bool page_fault = false;
     string res = "";
 
     switch (algorithm) {
     case OPT:
-	/* TODO */
-	printf("!");
+	next = next_OPT;
 	break;
     case LRU:
-	/* TODO */
+	next = next_LRU;
 	break;
     case FIFO:
 	next = next_FIFO;
 	break;
     case CLOCK:
-	/* TODO */
+	next = next_CLOCK;
 	break;
     }
 
+    /* check all page references */
     for (int i = 0; i < references.size(); ++i) {
-	if (frames.size() < num_frames) {
-	    /* there are empty frames */
-	    frames.push_back(references[i]);
-	} else if (find(frames.begin(), frames.end(), references[i]) == frames.end()) {
-	    page_fault = true;
-	    frames[next_index] = references[i];
-	    next_index = next(next_index);
+	if (s.find(references[i]) == s.end()) {
+	    /* page fault */
+	    s.insert(references[i]);
+
+	    if (frames.size() < num_frames) {
+		/* there are empty frames */
+		replace_index++;
+		frames.push_back(references[i]);
+	    } else {
+		replace_index = next(replace_index, i);
+		page_fault = true;
+		s.erase(frames[replace_index]);
+		frames[replace_index] = references[i];
+	    }
 	}
-	pframe(i, page_fault);
+	/* need to set the use bit for clock policy */
+	if (algorithm == CLOCK) {
+	    usebits[find(frames.begin(), frames.end(), references[i]) - frames.begin()] = true;
+	}
+	
+	print_frame(i, page_fault);
+	if (page_fault) {
+	    page_fault = false;
+	}
     }
 }
 
-void pframe(int i, bool page_fault) {
+/* print the frame to standard out */
+void print_frame(int i, bool page_fault) {
     	printf("%d: [", references[i]);
 	for (int i = 0; i < num_frames; ++i) {
 	    if (i < frames.size() && frames[i] < 10) {
@@ -91,17 +118,69 @@ void pframe(int i, bool page_fault) {
 	    }
 	}
 	if (page_fault) {
-	    page_fault = false;
 	    printf(" F\n");
 	} else {
 	    printf("\n");
 	}
 }
 
-int next_FIFO(int cur_index) {
+/* FIFO policy */
+int next_FIFO(int cur_index, int ref_index) {
     return (cur_index + 1) % num_frames;
 }
 
+/* Clock policy */
+int next_CLOCK(int cur_index, int ref_index) {
+    cur_index = (cur_index + 1) % num_frames;
+    while (usebits[cur_index]) {
+	usebits[cur_index] = false;
+	cur_index = (cur_index + 1) % num_frames;
+    }
+    return cur_index;
+}
+
+/* OPT policy */
+int next_OPT(int cur_index, int ref_index) {
+    int longest_time = 0, longest_index = 0;
+    
+    for (int i = 0; i < frames.size(); ++i) {
+	for (int j = ref_index + 1; j < references.size(); ++j) {
+	    if (frames[i] == references[j]) {
+		if (j > longest_time) {
+		    longest_time = j; // found a page whose time to the next reference is longer, update the time and index
+		    longest_index = i;
+		}
+		break;		    
+	    }
+	    if (j == references.size() - 1 && j > longest_time) {
+		longest_time = j; // also update if no reference is found in the future
+		longest_index = i;
+	    }
+	}
+    }
+    return longest_index;
+}
+
+/* LRU policy */
+int next_LRU(int cur_index, int ref_index) {
+    int least_time = ref_index, least_index = 0;
+    for (int i = 0; i < frames.size(); ++i) {
+	for (int j = ref_index - 1; j >= 0; --j) {
+	    if (frames[i] == references[j]) {
+		if (j < least_time) {
+		    least_time = j; // update time and index
+		    least_index = i;
+		}
+		break;
+	    }
+	    if (j == 0 && j < least_time) { // this is redundant, the frame must have been referenced
+		least_time = j;
+		least_index = i;
+	    }
+	}
+    }
+    return least_index;
+}
 
 int main(int argc, char **argv) {
     if (argc < 4) {
@@ -122,6 +201,7 @@ int main(int argc, char **argv) {
 	algorithm = FIFO;
     } else if (algo == "CLOCK" || algo == "clock") {
 	algorithm = CLOCK;
+	usebits = vector<bool>(num_frames, false);
     } else {
 	fprintf(stderr, "invalid algorithm\n");
 	return 1;
